f5119b3ae408c774d0d90c1b7b2f6aaeb03dbd6c
kranthi318 <chevuru.kranthikumar@gmail.com>
2018-02-24 14:23:11 +0530

    updated bank_app_form

---
+++ bank_app_form.sh
@@ -0,0 +1,56 @@
+clear
+
+
+
+
+
+
+echo -e "\n\t---------- Welcome My Bank-----------" 
+echo -e "\n\t ------ My SBI Bank Application From -------"
+echo -e "\t --------------------o0o-------------------"
+read -p " Enter the First Name : " first
+#if [ -#$ $first ]
+  # then
+   #echo -e "\t First Name is not optional ... "
+   #echo -e "\t Please Enter First name ...."
+   #echo -e "\t Thank you Try again .... "
+   #fi
+read -p " Enter the Middle Name : " second 
+read -p " Enter Last Name : " last
+read -p " Set Your Account User Name : " user
+read -s -p " Set Your Account Valied Password : " passwd ;echo 
+read -p " Enter your valied Mobile Number : " mobile
+read -p  "Enter your valied E-mail id [ @gmail ] : " mail
+read -p " Enter Your village/Town Name : " village
+read -p " Enter Your Mandal Name : " mandal
+read -p " Enter Your District Name : " dis
+read -p " Enter your State : " state 
+read -p " Enter Your Postal-Pin Number : " post
+
+
+
+
+clear
+
+echo -e " \n\n \t......... Welcome To SBI Bank ......... "
+echo -e "\n \t ....... Applicent Details .........\n"
+echo -e " \t First Name of the Applicent : $first "
+echo -e " \t Second Name of the Applicent : $second "
+echo -e " \t Last Name of the Applicent : $last "
+echo -e " \t Full Name Of The Applicent : $first $middle $last \n"
+echo -e " \t creditionals of the Applicent .... "
+echo -e " \t\t User Name of the Applicent  : $user "
+echo -e " \t\t Password of the Applicent   : $passwd \n"
+echo -e " \t Applicent Phone Number : +91-$mobile "
+echo -e " \t Applicent E-Mail Id : $mail \n"
+echo -e " \t Applicent Address.... \n "
+echo -e " \t\t Applicent Village Name      : $village "
+echo -e " \t\t Applicent Mandal Name       : $mandal "
+echo -e " \t\t Applicent District Name     : $dis "
+echo -e " \t\t Applicent State Name        : $state "
+echo -e " \t\t Applicent Postal-Pin Number : $post "
+
+echo -e " \n\n\t Thank you for giving details .....\n\n\n"
+
+echo -e "\n ...Visit again ......"
+
